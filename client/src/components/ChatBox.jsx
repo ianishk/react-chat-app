3fb644f3955be1b8962a1480034c1e8932c49ac7
Anish <anishrangan01@gmail.com>
2022-06-15 10:43:30 +0530

    minor changes with the chatbox header

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -6,14 +6,12 @@ import { useState, useEffect } from "react";
 
 import "../css/ChatBox.css";
 import ChatBubble from "./ChatBubble";
-const ChatBox = ({socket}) => {
-
+const ChatBox = ({ socket }) => {
   const selectedFriend = useSelector((state) => state.friend);
   const user = useSelector((state) => state.user);
   const [Messages, setMessages] = useState([]);
   const [Input, setInput] = useState("");
 
-  
   //useEffect to fetch the list of messages
 
   //content of the message:
@@ -33,47 +31,50 @@ const ChatBox = ({socket}) => {
   //   })
   //     .then((res) => res.json())
   //     .then((data) => {
-  //       
+  //
   //     });
   // }, []);
 
   useEffect(() => {
     setMessages([]);
-    fetch(`http://localhost:5000/message/getMessages?friendUsername=${selectedFriend.username}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-auth-token": `${localStorage.getItem("token")}`,
+    fetch(
+      `http://localhost:5000/message/getMessages?friendUsername=${selectedFriend.username}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": `${localStorage.getItem("token")}`,
+        },
       }
-    })
+    )
       .then((res) => res.json())
       .then((data) => {
-        
         setMessages(data.messages);
       });
   }, [selectedFriend]);
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      
-      if(data.sent === selectedFriend.username){
+      if (data.sent === selectedFriend.username) {
         const newData = {
           text: data.text,
           username: selectedFriend.username,
-        }
+        };
         setMessages([...Messages, newData]);
-      }
-      else{
+      } else {
         setMessages([]);
       }
-    })
+    });
   }, [socket, Messages, selectedFriend]);
-
-    
 
   const postHandler = (e) => {
     e.preventDefault();
-    socket.emit("post_message", {text: Input, sent: user.username, to: selectedFriend.username, chatId: selectedFriend.chatId});
+    socket.emit("post_message", {
+      text: Input,
+      sent: user.username,
+      to: selectedFriend.username,
+      chatId: selectedFriend.chatId,
+    });
     fetch("http://localhost:5000/message/addMessage", {
       method: "POST",
       headers: {
@@ -96,11 +97,15 @@ const ChatBox = ({socket}) => {
     <div className="chatBox__main">
       <div className="chatBox__header">
         <Stack direction="horizontal" gap={3}>
-          <Image
-            roundedCircle
-            height={60}
-            src={`https://avatars.dicebear.com/api/avataaars/${selectedFriend.profile}.svg`}
-          />
+          {selectedFriend.profile ? (
+            <Image
+              roundedCircle
+              height={60}
+              src={`https://avatars.dicebear.com/api/avataaars/${selectedFriend.profile}.svg`}
+            />
+          ) : (
+            <></>
+          )}
           <label>
             <b>{selectedFriend.username}</b>
           </label>
